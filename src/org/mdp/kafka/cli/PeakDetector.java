6718d9720a6ad4b39c0a1a26479941fe846bf707
Irozuku <emiliodu@gmail.com>
2024-06-27 20:35:17 -0400

    Finished PeakDetector

--- src/org/mdp/kafka/cli/PeakDetector.java
+++ src/org/mdp/kafka/cli/PeakDetector.java
@@ -3,7 +3,10 @@ package org.mdp.kafka.cli;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
 
@@ -14,13 +17,57 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.mdp.kafka.def.KafkaConstants;
 
 public class PeakDetector {
+	public static int FIFO_SIZE;
+	public static int EVENT_START_TIME_INTERVAL;
+	public static int EVEN_END_TIME_INTERVAL;
+	public static String start;
+	public static String finish;
+	public static String category = null;
+	public static List<List<String>> peakEvents = new ArrayList<>();
 	
+	public static final List<String> CATEGORIES = new ArrayList<>(Arrays.asList("accessories", 
+			"apparel", 
+			"appliances", 
+			"auto",
+			"computers",
+			"construction",
+			"country_yard",
+			"electronics",
+			"furniture",
+			"kids",
+			"medicine",
+			"unknown",
+			"sport",
+			"stationery"));;
+			
+			
 	public static void main(String[] args) throws FileNotFoundException, IOException{
-		if(args.length!=1){
-			System.err.println("Usage [inputTopic]");
+		if(args.length!=3 && args.length!=4){
+			System.err.println("Usage [eventType] [numEvents] [timePeak] [category*]");
 			return;
 		}
 		
+		try {
+			FIFO_SIZE = Integer.valueOf(args[1]);
+			EVENT_START_TIME_INTERVAL = Integer.valueOf(args[2]) * 1000;
+			EVEN_END_TIME_INTERVAL = 2 * EVENT_START_TIME_INTERVAL;
+			if (args.length == 4) {
+				if (!CATEGORIES.contains(args[4])) {
+					System.err.println("Not a valid category");
+					System.err.println(CATEGORIES.toString());
+		            return;
+				}
+				category = args[4];
+				
+			}
+			
+		} catch (NumberFormatException e) {
+            System.err.println("Invalid integer input for numEvents/timePeak");
+            return;
+        } 
+		
+		
+
 		Properties props = KafkaConstants.PROPS;
 
 		// randomise consumer ID so messages cannot be read by another consumer
@@ -30,6 +77,13 @@ public class PeakDetector {
 		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
 		
 		consumer.subscribe(Arrays.asList(args[0]));
+
+		// initialize a FIFO queue
+		LinkedList<ConsumerRecord<String, String>> fifo = new LinkedList<ConsumerRecord<String, String>>();
+
+		// initialize some state variables
+		boolean inEvent = false;
+		int events = 0;
 		
 		try{
 			while (true) {
@@ -38,10 +92,41 @@ public class PeakDetector {
 				
 				// for all records in the batch
 				for (ConsumerRecord<String, String> record : records) {
-					System.out.println(record.value());
+					if (category != null) {
+						String[] row = record.value().split(",");
+						if (!category.equals(row[4])) {
+							continue;
+						}
+					}
+					
+					fifo.add(record);
+
+					// check if we have FIFO_SIZE elements in the FIFO queue
+					if(fifo.size()>=FIFO_SIZE) {
+						// remove first element of queue
+						ConsumerRecord<String, String> oldest = fifo.removeFirst();
+						
+						// diference of timestamps
+						long gap = record.timestamp() - oldest.timestamp();
+						
+						// event condition
+						if(gap <= EVENT_START_TIME_INTERVAL && !inEvent) {
+							inEvent = true;
+							events++;
+							System.out.println("START event-id:" + events + ": start:"+oldest.timestamp() + " value:"+oldest.value() + " rate:" + FIFO_SIZE + " records in " + gap + " ms");
+							start = oldest.value().split(",")[0];
+						}
+						else if(gap >= EVEN_END_TIME_INTERVAL && inEvent) {
+							inEvent = false;
+							System.out.println("END event:" + events + ": finish:" + record.timestamp() + " value:" + record.value() + " rate:" + FIFO_SIZE + " records in " + gap + " ms");
+							finish = record.value().split(",")[0];
+							peakEvents.add(new ArrayList<>(Arrays.asList(start, finish)));
+						}	
+					}
 				}
 			}
 		} finally{
+			System.out.println(peakEvents.toString());
 			consumer.close();
 		}
 	}
